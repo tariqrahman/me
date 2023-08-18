62541052b79eb2afe7c1ea28cc0d28a3a470c7d1
Tariq Rahman <tariqrahman@g.ucla.edu>
2023-08-18 00:51:48 -0700

    fix casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home.jsx";
+import Home from "./components/home.jsx";
 import AboutMe from "./components/aboutMe.jsx";
 import Projects from "./components/projects.jsx";
 import Nav from "./components/nav";
 import Footer from "./components/footer.jsx";
-import ContactButton from "./components/contactButton.jsx";
 
 function App() {
   return (
